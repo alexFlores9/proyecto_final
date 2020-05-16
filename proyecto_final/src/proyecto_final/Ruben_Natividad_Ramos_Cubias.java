7792167bcb599862b7b54539d20d219dbd32ed9a
ruben-rc <65414663+ruben-rc@users.noreply.github.com>
2020-05-15 18:28:53 -0600

    Update Ruben_Natividad_Ramos_Cubias.java

--- proyecto_final/src/proyecto_final/Ruben_Natividad_Ramos_Cubias.java
+++ proyecto_final/src/proyecto_final/Ruben_Natividad_Ramos_Cubias.java
@@ -1,1 +1,15 @@
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_final;
+
+/**
+ *
+ * @author ruben
+ */
+public class Ruben_Natividad_Ramos_Cubias {
+    
+}
